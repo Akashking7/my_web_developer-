71f37f112d84f4f0a1844f23b82f190938145e5e
Akash <akashsubramani2002@gmail.com>
2026-02-25 13:03:21 +0530

    25-2-2026  intemidate-javascript

--- 6. JavaScript/Intemidate-JavaScript/Fibonacci Challenge.js
+++ 6. JavaScript/Intemidate-JavaScript/Fibonacci Challenge.js
@@ -1,30 +1,14 @@
-//Fibonacci Challenge
-
-//Fibonacci series:
-
-//0, 1, 1, 2, 3, 5, 8...
-
-function fibonacciGenerator(n) {
-    
+function fibonacciGenerator(n) { 
     var output = [];
-
-  if (n === 1) {
-    output = [0];
-  } else if (n === 2) {
-    output = [0, 1];
-  } else {
-    output = [0, 1];
-
-    for (var i = 2; i < n; i++) {
-      output.push(output[i - 2] + output[i - 1]);
+    if (n === 1) {
+        output = [1];
+    } else if (n == 2) {
+        output = [0, 1];
+    } else {
+        output = [0, 1];
+        for (var i = 2; i < n; i++){
+            output.push(output[i - 2] + output[i - 1]);
+        }
     }
-  }
-
-  return output;
+    return output;
 }
-
-console.log(fibonacciGenerator(6));
-
-Output:
-
-[0,1,1,2,3,5]
